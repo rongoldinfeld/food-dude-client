0624099c651d85df69196c483c79cbaaf49f299f
Ron Goldinfeld <rongoldinfeld56@gmail.com>
2020-12-13 16:07:26 +0200

    fix: routing

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import RestaurantsFeed from './restaurants-feed/restaurants-feed'
+import Restaurant from './restaurant/restaurant'
+
+export default function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path={`/restaurants/:id`}>
+          <Restaurant />
+        </Route>
+        <Route path="/restaurants">
+          <RestaurantsFeed />
+        </Route>
+        <Route path="/" exact={true} render={() => <Redirect to={'/restaurants'} />} />
+      </Switch>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import App from './App'
 import './index.css'
 import Navbar from './navbar/navbar'
-import RestaurantsFeed from './restaurants-feed/restaurants-feed'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route path="/restaurants">
-          <RestaurantsFeed />
+        <Route path="/">
+          <App />
         </Route>
-        <Route exact={true} path="/" render={() => <Redirect to="/restaurants" />} />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
